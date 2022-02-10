9231b174aaf179a851574f8e6baab8681709bc83
Randie Pathirage <randiepathirage@gmail.com>
2022-02-10 12:32:58 +0530

    Customize swagger UI

--- src/main/java/com/example/API/ApiApplication.java
+++ src/main/java/com/example/API/ApiApplication.java
@@ -2,6 +2,12 @@ package com.example.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,5 +17,21 @@ public class ApiApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerConfig() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.API"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder().title("Sample SSE API")
+                .build();
     }
 }
